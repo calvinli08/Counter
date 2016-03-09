f174461ce9848e5badc24443dea3ba97a5b7b6a4
Calvin <cli.calvinli@gmail.com>
2016-03-08 21:28:47 -0500

    Added javadoc to Counter class

--- src/intern/Counter.java
+++ src/intern/Counter.java
@@ -1,20 +1,48 @@
-/* This is a Java counter class. It increments and decrements a counter in intervals
- * of one unit; either hour, minute, or second.
- * Written by Calvin Li.
- * Written on Ubuntu Linux, Eclipse Luna, Java 8 SE.
- */
+/* A Java counter class requested by Maxeta's Intern Project. */
 
 package intern;
 
+/** Implements the Counter class specified in the Intern Project Requirements.
+ * The counter increments and decrements itself in intervals of one unit;
+ * either hour, minute, or second.
+ * <p>
+ * Written on Ubuntu Linux, Eclipse Luna, Java 8 SE.
+ * <br>
+ * Compiled using javac.
+ * <p>
+ * References:
+ * <br>
+ * 1) Javadoc command reference page:
+ * <br>
+ * https://docs.oracle.com/javase/7/docs/technotes/tools/windows/javadoc.html#synopsis
+ * <br>
+ * 2) Javadoc formatting tutorial page:
+ * <br>
+ * http://www.oracle.com/technetwork/articles/java/index-137868.html
+ * <br>
+ * 3) Java 8 API:
+ * <br>
+ * https://docs.oracle.com/javase/8/docs/api/
+ * <br>
+ * 4) HTML for use in javadoc:
+ * <br>
+ * http://www.w3schools.com/html/html_paragraphs.asp
+ *
+ * @author Calvin Li
+ * @version 1.0
+ * */
 public class Counter {
 
-	/* Store the counter components in an array for easy access (H/M/S)
-	 * count[0] = hour ; count[1] = minute ; count[2] = second */
-	int[] count = new int[3];
+	/** The count array stores the counter components for easy access (H:M:S).
+	 * count[0] = hour ; count[1] = minute ; count[2] = second
+	 * */
+	private int[] count = new int[3];
 
-	/* Default Constructor
-	 * The default constructor sets the counter to 0:0:0 */
-	Counter()
+	/** Default Constructor
+	 * <p>
+	 * Sets the counter to 0:0:0
+	 * */
+	public Counter()
 	{
 		/* Set the fields to zero */
 		count[0] = 0;
@@ -22,68 +50,101 @@ public class Counter {
 		count[2] = 0;
 	}
 
-	/* Parameterized Constructor sets the counter to the values specified in h,m,s*/
-	Counter(int h, int m, int s)
+	/** Parameterized Constructor
+	 * <p>
+	 * Initializes the counter at the time specified by h,m, and s.
+	 * Constructor checks if the values of h, m, and s are
+	 * within bounds; the bounds for each being:
+     * <br>
+	 * h ranges from 0 to 23, inclusive;
+     * <br>
+	 * m ranges from 0 to 59, inclusive;
+     * <br>
+	 * s ranges from 0 to 59, inclusive.
+	 *
+	 * @param	h	value to initialize hours field to
+	 * @param	m	value to initialize minutes field to
+	 * @param	s	value to initialize seconds field to
+	 * */
+	public Counter(int h, int m, int s)
 	{
-		/* If input is within bounds */
+		/** Checks if input is within bounds */
 		if (h <= 23 && h >= 0 && m <= 59 && m >= 0 && s <= 59 && s >= 0)
 		{
 			count[0] = h;
 			count[1] = m;
 			count[2] = s;
 		}
-		/* Display an error message and set counter to 0:0:0 in event of undesirable input */
+		/** Auto sets counter to 0:0:0 in event of undesirable input.*/
 		else
 		{
-			//System.out.println("A negative or out of bound number was entered. Auto setting counter to 0:0:0 instead.\n");
 			count[0] = 0;
 			count[1] = 0;
 			count[2] = 0;
 		}
 	}
 
-	/* Get functions for each field in the counter. Set functions are not provided
-	 * because the project instructions requested that the only allowable method
-	 * to change the Counter time after initialization be incrementing/decrementing each field. */
-
+	/** Gets an array containing the counter's current time.
+	 * <p>
+	 * Set functions are not provided because the project instructions
+	 * requested that the only allowable method to change the Counter
+	 * time after initialization be incrementing/decrementing each field.
+	 *
+	 * @return integer array containing current time
+	 * */
 	public int[] get()
 	{
-		return count;
+        int[] time = count;
+        return time;
 	}
 
-	/* Display standard time */
-	public void displayStandard()
+	/** Displays the counters current time in Standard 12-hour format.
+	 * <p>
+	 * Since most display mediums can be expected to use strings,
+	 * strings with format tags are also provided.
+	 *
+	 * @return	String containing current counter time in Standard 12-hour format
+	 * */
+	public String displayStandard()
 	{
-		/* Check if it is the morning or the afternoon
-		 * and print the appropriate 12-hour time with AM/PM marker */
-		if (count[0] > 12)
-		{
-			System.out.format("%tl:%tM:%tS PM" , (count[0] - 12), count[1], count[2]);
-		}
-		else
-		{
-			System.out.format("%tl:%tM:%tS AM" , count[0], count[1], count[2]);
-		}
-
-		/* For use in other applications that require a string */
-		String standardTimeAM = String.format("%tl:%tM:%tS AM", count[0], count[1], count[2]);
-		String standardTimePM = String.format("%tl:%tM:%tS PM", (count[0] - 12), count[1], count[2]);
-
+		/** Checks if it is the morning or the afternoon
+		 * and prints the appropriate 12-hour time with AM/PM marker
+		 * to standard output.
+		 * Also returns Strings containing time. */
+			if (count[0] > 12)
+			{
+				System.out.format("%tl:%tM:%tS PM" , (count[0] - 12), count[1], count[2]);
+				return String.format("%tl:%tM:%tS PM", (count[0] - 12), count[1], count[2]);
+			}
+			else
+			{
+				System.out.format("%tl:%tM:%tS AM" , count[0], count[1], count[2]);
+				return String.format("%tl:%tM:%tS AM", count[0], count[1], count[2]);
+			}
 	}
 
-	/* Print military time */
-	public void displayMilitary()
+	/** Displays the counter's current time in Military 24-hour format.
+	 * <p>
+	 * Since most display mediums can be expected to use strings,
+	 * strings with format tags are also provided.
+	 *
+	 * @return	String containing current counter time in Military 24-hour format
+     */
+	public String displayMilitary()
 	{
-		/* Display military time */
+		/** Prints military time to standard output.
+		 * Also returns strings for use in other applications */
 		System.out.format("%tH:%tM:%tS" , count[0], count[1], count[2]);
-
-		/* For use in other applications that require a string */
-		String militaryTime = String.format("%tH:%tM:%tS");
+		return String.format("%tH:%tM:%tS");
 	}
 
-	/* Functions used to increment and decrement the counter */
+	/* Methods used to increment and decrement the counter */
 
-	/* Function to increment hour field */
+	/** Increases the value in the counter's hours field by one.
+	 * <p>
+	 * If the value of hours field before increment is equal to 23,
+	 * rolls over the counter to 0:0:0.
+	 *  */
 	public void incrementHour()
 	{
 		if (count[0] < 23 && count[0] >= 0 )
@@ -98,7 +159,12 @@ public class Counter {
         }
 	}
 
-	/* Function to increment minute field */
+	/** Increases the value in the counter's minutes field by one.
+	 * <p>
+	 * If value of minutes field before increment is equal to 59,
+	 * calls <code>incrementHour</code>. For example, incrementing 0:59:0 yields
+     * 1:0:0.
+	 * */
 	public void incrementMinute()
 	{
 		if (count[1] < 59 && count[1] >= 0)
@@ -112,7 +178,12 @@ public class Counter {
 		}
 	}
 
-	/* Function to increment second field */
+	/** Increases the value in the counter's seconds field by one.
+	 * <p>
+	 * If value of seconds field before increment is equal to 59,
+	 * calls <code>incrementMinute</code>. For example, incrementing 0:0:59
+     * yields 0:1:0.
+	 * */
 	public void incrementSecond()
 	{
 		if (count[2] < 59 && count[2] >= 0)
@@ -127,7 +198,11 @@ public class Counter {
 
 	}
 
-	/* Function to decrement hour field */
+	/** Decreases the value in the counter's hours field by one.
+	 * <p>
+	 * If value of hours field before decrement is equal to 0,
+	 * sets counter to 23:59:59.
+	 * */
 	public void decrementHour()
 	{
 		if (count[0] < 23 && count[0] >=1)
@@ -142,7 +217,12 @@ public class Counter {
 		}
 	}
 
-	/* Function to decrement minute field */
+	/** Decreases the value in the counter's minutes field by one.
+	 * <p>
+	 * If value of minutes field before decrement is equal to 0,
+	 * sets minutes and seconds field to 59, and calls <code>decrementHour</code>.
+     * For example, decrementing 2:0:25 yields 1:59:59.
+	 * */
 	public void decrementMinute()
 	{
 		if (count[1] < 59 && count[1] >=1)
@@ -157,7 +237,12 @@ public class Counter {
 		}
 	}
 
-	/* Function to decrement second field */
+	/** Decreases the value in the counter's seconds field by one.
+	 * <p>
+	 * If value of seconds field before decrement is equal to 0,
+	 * sets seconds field to 59, and calls <code>decrementMinute</code>.
+     * For example, decrementing 2:2:0 yields 2:1:59.
+	 * */
 	public void decrementSecond()
 	{
 		if (count[2] < 59 && count[2] >= 1)
